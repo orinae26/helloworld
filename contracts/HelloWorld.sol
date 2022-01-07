4e874120ed7fa4b0ea961f4e272ecb946b7879d1
Orina <oryna.rich26@gmail.com>
2022-01-07 18:49:18 +0300

    First App: Function to increment and Decrement

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -1,6 +1,29 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.10 <0.9.0;
 
-contract HelloWorld {
-  string public greet = "Hello World";
+contract Count{
+
+  uint public count;
+
+  //function to get the current count
+
+  function get() public view returns (uint){
+
+    return count;
+  }
+
+  //function to increment the count
+
+  function inc() public{
+
+    count ++;
+  }
+  //function to decrement the count
+
+  function dec() public{
+
+    count --;
+  
+  }
+  
 }
