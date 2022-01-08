c144e7b6d7c0b412ca3f3a4994cb09b093fba378
Orina <oryna.rich26@gmail.com>
2022-01-08 07:58:05 +0300

    constants

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -2,23 +2,14 @@
 //compiler 
 pragma solidity >=0.8.10 <0.9.0;
 
-contract Varibales{
-  //state variables are savedto the blockchain
+//constants are variables that cannot be modified
+//constants are hard coded
+//using constants can save on gas cost
 
-  string public text = "Hello World!";
-  uint public num = 12345;
+contract Constant{
+  //coding convention to uppercase constant variables
+  address public constant MY_ADDRESS = 0x0000000000000000000000000000000000000000;
+  uint public constant MY_UINT = 123;
 
-  function doSomething()public {
-    //local variables are not saved to the blockchain
-    uint i = 125;
-
-    //some global saved to the blockchain
-    
-    uint timestamp = block.timestamp; //current block timestamp
-    address sender = msg.sender;//current caller address
-
-  }
-
-
-  
+ 
 }
