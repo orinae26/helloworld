d982e41391def31b8a070fa3a0021274b72a0b65
Orina <oryna.rich26@gmail.com>
2022-01-08 07:50:07 +0300

    state and local variables

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -1,73 +1,24 @@
 // SPDX-License-Identifier: MIT
+//compiler 
 pragma solidity >=0.8.10 <0.9.0;
 
-contract Primitive{
-  bool public b = true;
-  //uint type cannnot take -ve value
-  uint public u = 1;
-  uint8 public u8 = 1;
-  uint128 public u128 = 25;
-  uint256 public u256 = 299;
+contract Varibales{
+  //state variables are savedto the blockchain
 
-  //int type can take a +value
-  int public i = -1;
-  int8 public i8 = 8;
-  int128 public i128 = 25;
-  int256 public i256 = 255;
+  string public text = "Hello World!";
+  uint public num = 12345;
 
-  address public addr = 0x1234567890123456789012345678901234567890;
+  function doSomething()public {
+    //local variables are not saved to the blockchain
+    uint i = 125;
 
-    //min and max of int
-
-  int public minInt = type(int).min;
-  int public maxInt = type(int).max;
-
-  //bytes:shorthand of byte[]
-  bytes1 a = 0xb5; //[10110101]
-  
-  //default values
-  address public defaultAddr = 0x0000000000000000000000000000000000000000;
-  int public defaultInt = 0;
-  uint public defaultUint = 0;
-
-  //mapping
-  mapping(address => uint) public balances;
-  mapping(address => mapping(address => uint)) public nestedBalances;
-
-
-  //array
-  uint8[2] public a8 = [1,2];
-
-  //struct
-  struct S{
-    uint8 a;
-    uint8 b;
-  }
-  S public s = S(1,2); //set value
-
-  //event
-  event E(address indexed a, uint indexed b);
-  event E2(address indexed a, uint indexed b, uint indexed c);
-
-
-  //modifier
-  modifier mod1{
-    if(msg.sender == 0x1234567890123456789012345678901234567890){
-      _;
-    }
-
-
-
-
-
-
+    //some global saved to the blockchain
+    
+    uint timestamp = block.timestamp; //current block timestamp
+    address sender = msg.sender;//current caller address
 
   }
 
+
   
-
-
-
-
-
 }
