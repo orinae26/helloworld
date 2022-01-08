8bea56e659e7bf9a6fd000e08c8896c364fd695d
Orina <oryna.rich26@gmail.com>
2022-01-08 21:14:30 +0300

    gas, if else statements

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -1,17 +1,37 @@
 // SPDX-License-Identifier: MIT
 //compiler btwn 0.8.10 and 0.9.0
 pragma solidity >=0.8.10 <0.9.0;
+// gas for transaction = gas price* gas spent
+//gas spent: total amount of gas spent in transaction
+//gas price: price of gas per unit of gas 
 
-  contract EtherUnits{
+contract Gas{
+  uint public i = 0;
 
-    uint public oneWei = 1 wei;
+  //using up all gas in a transaction causes the transaction to fail
+  //state changes are not reverted
+  //gas is not refunded
+  function forever () public {
+    while(true) {
+      i++;
+    }
+  }  
+}
 
-    bool public isOneWei = 1 wei ==1;
-
-    uint public oneEther = 1 ether;
-
-    // 1 ether is equal to 10^18 wei
-
-    bool public isOneEther = 1 ether == 1^18;
-
+contract ifElse {
+  function foo(uint x) public pure returns (uint){
+    if (x<10){
+      return 0;
+    }
+    else if(x<20){
+      return 1;
+    }
+    else {
+      return 2;
+    }
   }
+  //ternary operator
+  function ternary(uint _x)public pure returns(uint){
+    return _x<10 ? 0 : _x<20 ? 1 : 2;
+  }
+}
