4ea94037ba4e497b8d11dc6bbdbbf751b8743e13
Orina <oryna.rich26@gmail.com>
2022-01-10 17:24:25 +0300

    inheritance: function virtual, function override

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -518,8 +518,135 @@ contract E is X,Y{
   constructor () Y("Y was called") X("X was called"){}
 }
 
+/*Graph of inheritance
 
+        A 
+       / \
+      B  C
+      /\ /\
+    F  D   E
 
+    A: Base contracc
+
+    B and C: Inherits from A
+    D: Inherits from B, C
+    E: Inherits from A, C
+    F: Inherits from B,A
+    */
+
+/*
+//contracts can inherit other contracts by using is keyword 
+//fn that is going to be overidden by a child contract must must be declared virtual
+//fn that is going to override a parent function must use override keyword
+//order of inheritance is important
+//list parent contracts in the order from "most base like " to "most derived like"
+contract A{
+  function foo() public pure virtual returns (string memory ){
+    return "A";
+  }
+
+}
+
+//contract B inherits from A
+contract B is A {
+  //override foo function
+  function foo()public pure virtual override returns (string memory ){
+    return "B";
+  }
+}
+
+//contract C inherits from A
+contract C is A {
+  //overide foo function
+  function foo()public pure virtual override returns (string memory){
+    return "C";
+
+  }
+}
+contract D is B,C{
+  //D.foo() returns "C"
+  //since C is the right-most parent contract with function foo()
+  function foo() public pure override (B,C) returns (string memory){
+    return super.foo();
+  }
+}
+
+contract E is C,B{
+  function foo() public pure override (C,B) returns (string memory){
+    return super.foo();
+  }
+}
+
+//iheritance must be ordered  from most-base-like to most-derived
+//swapping order of A and B will throw a compilation error
+contract F is A,B{
+  function foo() public pure override(A,B) returns (string memory){
+    return super.foo();
+  }
+}
+
+*/
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+/* Graph of inheritance
+    A
+   / \
+  B   C
+ / \ /
+F  D,E
+
+*/
+
+contract A {
+    function foo() public pure virtual returns (string memory) {
+        return "A";
+    }
+}
+
+// Contracts inherit other contracts by using the keyword 'is'.
+contract B is A {
+    // Override A.foo()
+    function foo() public pure virtual override returns (string memory) {
+        return "B";
+    }
+}
+
+contract C is A {
+    // Override A.foo()
+    function foo() public pure virtual override returns (string memory) {
+        return "C";
+    }
+}
+
+// Contracts can inherit from multiple parent contracts.
+// When a function is called that is defined multiple times in
+// different contracts, parent contracts are searched from
+// right to left, and in depth-first manner.
+
+contract D is B, C {
+    // D.foo() returns "C"
+    // since C is the right most parent contract with function foo()
+    function foo() public pure override(B, C) returns (string memory) {
+        return super.foo();
+    }
+}
+
+contract E is C, B {
+    // E.foo() returns "B"
+    // since B is the right most parent contract with function foo()
+    function foo() public pure override(C, B) returns (string memory) {
+        return super.foo();
+    }
+}
+
+// Inheritance must be ordered from “most base-like” to “most derived”.
+// Swapping the order of A and B will throw a compilation error.
+contract F is A, B {
+    function foo() public pure override(A, B) returns (string memory) {
+        return super.foo();
+    }
+}
 
 
 
