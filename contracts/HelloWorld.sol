800433138d9a3d284e16c655499c043f7592552f
Orina <oryna.rich26@gmail.com>
2022-01-13 20:34:58 +0300

    Try/catch: for extranl contracts/functions

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -1143,8 +1143,66 @@ contract Carfactory{
   }
 }
 
+//try catch
+//try catch is used to catch errors from external function calls and contract creation
 
+contract Foo{
+  address public owner;
+  //constructor
+  constructor (address _owner){
+    owner = _owner;
+  require (owner != address(0)_"Invalid address");
+  assert (owner != 0x0000000000000000000000000000000000000001);
+  owner = _owner;
+  }
+  //function to call external function
+  function myFunc(uint x)public pure returns (string memory){
+    require (x != 0, "require failed");
+    return "my function was called";
+  }
+}
+contract Bar{
+  //function to call external function
+  event Log(string message);
+  event LogBytes(bytes, data)
+  Foo public foo;
 
+  //constructor
+  constructor{
+    //this Foo contract is used for testing try catch with an external function call
+    foo = new Foo(msg.sender);
+  }
+  //try/catch 
+  //example of try/catch with external function 
+  // tryCatchExternalCall(0) => Log("external call failed")
+  // tryCatchExternalCall(1) => Log("my func was called")
+
+  function tryCatchExternalCall(uint _i) public{
+    try foo.myFunc(_i) returns (string memory result) {
+      emit Log(result);
+    }
+    catch {
+      emit Log("External call Failed");
+    }
+  }
+  //example of try/catch with contract creation
+  // Example of try / catch with contract creation
+  // tryCatchNewContract(0x0000000000000000000000000000000000000000) => Log("invalid address")
+  // tryCatchNewContract(0x0000000000000000000000000000000000000001) => LogBytes("")
+  // tryCatchNewContract(0x0000000000000000000000000000000000000002) => Log("Foo created")
+  
+  function tryCatchNewContract (address _owner) public{
+    try new Foo(_owner) returns (Foo foo){
+      emit Log ("Foo created");
+    } catch Error (string memory reason) {
+      //catch failing revert() and require()
+      emit Log(reason);
+    } catch(bytes memory reason) {
+      //catch failing assert()
+      emit LogBytes (reason);
+    }
+  }
+}
 
 
 
